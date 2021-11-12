068cab46a073ab7e87d53749f21c17d0d33b1154
Michael DeMarco <michaelfromyeg@gmail.com>
2021-11-11 23:06:08 -0800

    feat: first draft done

---
+++ examples/basic/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("H%dllo, w%drld!\n", 3, 0);
+    return 0;
+}
---
+++ examples/java/Main.java
@@ -0,0 +1,7 @@
+package examples.java;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+    }
+}
